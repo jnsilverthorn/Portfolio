840d58541ee2bbe7dd147a4b671d04f72790e32a
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2019-11-11 16:13:07 -0800

    basic stylization and linked firebase

---
+++ assets/javascript/index.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyAoAjOxjSWNgNXPdgsk3qXGogv7-WV4ESo",
+    authDomain: "portfolio-d2735.firebaseapp.com",
+    databaseURL: "https://portfolio-d2735.firebaseio.com",
+    projectId: "portfolio-d2735",
+    storageBucket: "portfolio-d2735.appspot.com",
+    messagingSenderId: "1089230156015",
+    appId: "1:1089230156015:web:432cab140bbe0b12376b1a"
+}
+
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
