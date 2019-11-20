5270811cd1b4c296c2eeb739a91b00595c809da3
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2019-11-19 16:40:37 -0800

    all the html and images, some js, most css done

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -11,3 +11,49 @@ var config = {
 firebase.initializeApp(config);
 
 var db = firebase.firestore();
+
+const aboutGrab = $("#aboutNav");
+const portfolioGrab = $("#portfolioNav");
+const contactGrab = $("#contactNav");
+const resumeGrab = $("#resumeInfo");
+
+$(".container1").css("display", "block");
+
+aboutGrab.on("click", function () {
+    $(".container1").css("display", "block");
+    $(".container2").css("display", "none");
+    $(".container3").css("display", "none");
+    $(".container4").css("display", "none");
+})
+
+portfolioGrab.on("click", function () {
+    $(".container2").css("display", "block");
+    $(".container1").css("display", "none");
+    $(".container3").css("display", "none");
+    $(".container4").css("display", "none");
+})
+
+contactGrab.on("click", function () {
+    $(".container3").css("display", "block");
+    $(".container2").css("display", "none");
+    $(".container1").css("display", "none");
+    $(".container4").css("display", "none");
+})
+
+resumeGrab.on("click", function () {
+    $(".container3").css("display", "none");
+    $(".container2").css("display", "none");
+    $(".container1").css("display", "none");
+    $(".container4").css("display", "block");
+})
+
+$("#headerName").mouseenter(function () {
+    $("#headerName").text("").append(
+        "<a href='https://github.com/jnsilverthorn' target='_blank'><img class='iconImg' alt='github' src='assets/images/GitHub.png' width=35px height=35px></a>" +
+        "<a href='https://www.linkedin.com/in/james-silverthorn-104b01192/' target='_blank'><img class='iconImg' alt='linkedIN' src='assets/images/linked.png' width=35px height=35px></a>" +
+        "<a href='https://www.facebook.com/james.silverthorn.5' target='_blank'><img class='iconImg' alt='facebook' src='assets/images/fb.png' width=35px height=35px></a>" +
+        "<a href='https://www.instagram.com/jnsilverthorn/' target='_blank'><img class='iconImg' alt='insta' src='assets/images/instagram.jpg' width=35px height=35px></a>"
+    )
+}).mouseleave(function () {
+    $("#headerName").text("James Silverthorn")
+})
